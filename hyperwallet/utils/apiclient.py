d8d2661d4632b5761edc28fd4d6869375ffdb7e3
Dan Harrison <hwyfour@gmail.com>
2016-09-20 15:49:01 -0700

    Added the doPost function. Uses TLS 1.2 to access the API.

--- hyperwallet/utils/apiclient.py
+++ hyperwallet/utils/apiclient.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+import ssl
+import json
+import requests
+
+from requests.auth import HTTPBasicAuth
+from requests_toolbelt.adapters.ssl import SSLAdapter
+
+
 from hyperwallet import __version__
 
 
@@ -12,6 +20,19 @@ class ApiClient(object):
     :param server: The target URL to submit HTTP requests.
     """
 
+    """
+    The current version of this SDK.
+    """
+    version = __version__
+
+    """
+    The custom User Agent to identify this client as our SDK.
+    """
+    baseHeaders = {
+        "User-Agent": "Hyperwallet Python SDK v{}".format(version),
+        "Accept": "application/json"
+    }
+
     def __init__(self, username, password, server):
         """
         Create an instance of the API client.
@@ -21,4 +42,42 @@ class ApiClient(object):
         self.username = username
         self.password = password
         self.server = server
-        self.version = __version__
+
+    def doPost(self, partialUrl, data, params = {}):
+        """
+        Submit a POST to the server.
+        Returns the response.
+        """
+
+        headers = self.baseHeaders
+        headers["Content-Type"] = "application/json"
+
+        with requests.Session() as s:
+            s.mount(self.server, SSLAdapter(ssl.PROTOCOL_TLSv1_2))
+
+            response = s.post(
+                # TODO need to join server safely
+                self.server + "/rest/v3/" + partialUrl,
+                data = json.dumps(data).encode('utf-8'),
+                params = params,
+                headers = headers,
+                auth = (self.username, self.password)
+            )
+
+            return response
+
+    def doPut(self, partialUrl, data, params = {}):
+        """
+        Submit a PUT to the server.
+        Returns the response.
+        """
+
+        pass
+
+    def doGet(self, partialUrl, params = {}):
+        """
+        Submit a GET to the server.
+        Returns the response.
+        """
+
+        pass
