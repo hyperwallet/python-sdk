750bef465839c2cfbc046d499ee684877584db60
Dan Harrison <dharrison@hyperwallet.com>
2017-08-25 16:16:17 -0700

    Renamed Balances Tests

--- hyperwallet/tests/test_api.py
+++ hyperwallet/tests/test_api.py
@@ -891,14 +891,14 @@ class ApiTest(unittest.TestCase):
 
         self.assertTrue(response[0].currency, self.balance.get('currency'))
 
-    def test_list_repaid_card_balances_with_nothing(self):
+    def test_list_prepaid_card_balances_with_nothing(self):
 
         with self.assertRaises(HyperwalletException) as exc:
             self.api.listBalancesForPrepaidCard()
 
         self.assertEqual(exc.exception.message, 'userToken is required')
 
-    def test_list_repaid_card_balances_with_user_token(self):
+    def test_list_prepaid_card_balances_with_user_token(self):
 
         with self.assertRaises(HyperwalletException) as exc:
             self.api.listBalancesForPrepaidCard('token')
@@ -906,7 +906,7 @@ class ApiTest(unittest.TestCase):
         self.assertEqual(exc.exception.message, 'prepaidCardToken is required')
 
     @mock.patch('hyperwallet.utils.ApiClient._makeRequest')
-    def test_list_repaid_card_balances_with_user_token_and_card_token(self, mock_get):
+    def test_list_prepaid_card_balances_with_user_token_and_card_token(self, mock_get):
 
         mock_get.return_value = {'data': [self.balance]}
         response = self.api.listBalancesForPrepaidCard('token', 'token')
