a73dd8fc362b1f89a39793c76a1dcb49a945d42b
mgovindhasamy <mgovindhasamy@paypal.com>
2020-11-18 11:36:31 +0530

    remove space

--- hyperwallet/tests/test_api.py
+++ hyperwallet/tests/test_api.py
@@ -2395,9 +2395,7 @@ class ApiTest(unittest.TestCase):
         self.assertTrue(response.token, self.data.get('token'))
 
     '''
-    
-        Get Transfer Refund
-        
+        Get Transfer Refund 
     '''
 
     def test_get_transfer_refund_fail_need_transfer_token(self):
