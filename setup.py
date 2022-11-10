8be4f3766e0995f07dfeb6948b6583024678488d
Gustavo Meyer <grmeyer@paypal.com>
2022-11-09 23:41:42 -0800

    DTSERWONE-846 Comment lint

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     packages = find_packages(exclude = ('tests', 'doc')),
     install_requires = ['requests', 'requests-toolbelt', 'jwcrypto', 'python-jose'],
     python_requires=">= 3.7",
-    tests_require = [ 'mock', 'nose2'],
+    tests_require = [ 'mock', 'nose'],
     keywords='hyperwallet api',
     classifiers=[
         'Development Status :: 4 - Beta',
