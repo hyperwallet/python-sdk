315b59f39294a1ca3012ec4990aac2ecce0b84d1
Gustavo Meyer <grmeyer@paypal.com>
2023-05-05 16:58:04 -0700

    DTONBWTWO-1073 Run CI/CD on dependabot

--- build/lib/hyperwallet/config.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-'''Sane defaults for accessing the Hyperwallet API'''
-
-SERVER = 'https://api.sandbox.hyperwallet.com'
--- build/lib/hyperwallet/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-from .apiclient import ApiClient
